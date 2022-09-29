78919dfa5c46427a0a6ba1b68bd59e5f6e536143
UBISOFT-ORG\abryan <adam.bryan@ubisoft.com>
2022-09-29 09:16:32 +0100

    testing it is still running after shut down

--- Support Bank.js
+++ Support Bank.js
@@ -1,7 +1,9 @@
 console.log("HELLO WORLD!!")
 <<<<<<< HEAD
 
-console.log("added by adam")
+console.log("added by adam");
 =======
 console.log("HELLO 2")
 >>>>>>> b676ec49193ab7cac242e33126e896b6a7435d15
+
+console.log("testing after shut down to test it is still running");
